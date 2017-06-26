7f7c63b0d0d93ddb4e926c79f6b8b1118dd3191d
Dominic Böck <dominicboeck@hotmail.com>
2017-06-27 00:52:24 +0200

    Add header in html and css

--- index.php
+++ index.php
@@ -3,8 +3,30 @@
   <head>
     <meta charset="utf-8">
     <title>projectAIM</title>
+    <link rel="stylesheet" href="css/style.css">
+	  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
   <body>
-    <span>projectAIM</span>
+    <!-- HEADER START -->
+    <div class="header">
+      <div class="header-inner">
+        <div class="header-logo">
+          <a href="#">projectAIM</a>
+        </div>
+        <div class="header-nav">
+          <a href="#">Play</a>
+          <a href="#">Leaderboard</a>
+          <a href="#">News</a>
+          <a href="#">About</a>
+          <a href="#">Help</a>
+          <a href="#">Donate</a>
+        </div>
+        <div class="header-login">
+          <a href="#">Login <span class="fa fa-sign-in"></span></a>
+        </div>
+      </div>
+    </div>
+    <!-- HEADER END -->
   </body>
 </html>
